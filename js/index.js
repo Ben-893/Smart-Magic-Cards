f537db546da2b6334522b6087653ba274e09e11f
Ben-893 <ben.smith.gordon@gmail.com>
2020-10-16 15:30:49 +0100

    Rendered the full suit to the page

--- js/index.js
+++ js/index.js
@@ -1,27 +1,35 @@
-const suit = 'hearts';
+const suit = ['hearts', 'spades', 'clubs', 'diamonds'];
 const cardsWrapper = document.querySelector('.cards-wrapper');
-const btnWrapper = document.querySelector('.btn-wrapper'); /* eslint-disable-line */
-const selectedCardsWrapper = document.querySelector('.selected-cards'); /* eslint-disable-line */
+const btnWrapper = document.querySelector(
+  '.btn-wrapper'
+); /* eslint-disable-line */
+const selectedCardsWrapper = document.querySelector(
+  '.selected-cards'
+); /* eslint-disable-line */
 
 function createCards() {
   const cards = [];
   // Create an array with objects containing the value and the suit of each card
-  for (let i = 1; i <= 13; i += 1) {
-    const cardObject = {
-      value: i,
-      suit,
-    };
-    cards.push(cardObject);
+  for (let i = 0; i < suit.length; i++) {
+    for (let x = 1; x <= 13; x++) {
+      const cardObject = {
+        value: x,
+        suit: suit[i],
+      };
+      cards.push(cardObject);
+    }
   }
+
+  console.log(cards);
 
   // For each dataObject, create a new card and append it to the DOM
   cards.forEach((card, i) => {
     const positionFromLeft = i * 15;
-    const cardElement = document.createElement('div');
-    cardElement.setAttribute('data-value', card.value);
-    cardElement.classList.add('card', `${card.suit}-${card.value}`);
-    cardElement.style.left = `${positionFromLeft}px`;
-    cardsWrapper.append(cardElement);
+    const cardElement = document.createElement('div'); //Creates a div
+    cardElement.setAttribute('data-value', card.value); // Sets the attribute of the newly created div to a number associated with that card
+    cardElement.classList.add('card', `${card.suit}-${card.value}`); // Sets the class of the newly created div equal to the suit-number of the card
+    cardElement.style.left = `${positionFromLeft}px`; // Manipulates the positioning of a div 'card'
+    cardsWrapper.append(cardElement); // Appends each new div 'card' to the cardsWrapper div
   });
 }
 
